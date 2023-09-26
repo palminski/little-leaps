142b2d9380a0f366c621b0aeb53ee99667c2b0c3
palminski <williambolls98@gmail.com>
2023-09-26 13:22:34 -0700

    collisions work in both x and y dimensions

--- Assets/Scripts/MovementCollisionHandler.cs
+++ Assets/Scripts/MovementCollisionHandler.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class MovementCollisionHandler : MonoBehaviour
 {
-
+    //Define
+    //====================================================================================================================
     private BoxCollider2D boxCollider;
 
     [SerializeField]
@@ -23,6 +24,7 @@ public class MovementCollisionHandler : MonoBehaviour
 
     private RaycastOrigins raycastOrigins;
 
+    //====================================================================================================================
     // Start is called before the first frame update
     void Start()
     {
@@ -33,12 +35,7 @@ public class MovementCollisionHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // DebugFunctions to check ray lengths
-        updateRaySpacing();
-        for (int i = 0; i < xRayCount; i++)
-        {
-            Debug.DrawRay(raycastOrigins.bottomRight + Vector2.up * xRaySpacing * i, Vector2.right * velocity.x, Color.magenta);
-        }
+
     }
     void FixedUpdate()
     {
@@ -50,19 +47,23 @@ public class MovementCollisionHandler : MonoBehaviour
         updateRaycastOrigins();
 
         CollisionsXAxis(ref velocity);
+        CollisionsYAxis(ref velocity);
 
         transform.Translate(velocity);
     }
 
     void CollisionsXAxis(ref Vector3 velocity)
     {
+        //Define some useful variables
         float xVelocity = velocity.x;
+        if (xVelocity == 0) return;
         float direction = Mathf.Sign(xVelocity);
         float distanceToCast = Mathf.Abs(xVelocity);
 
         Vector2 rayOrigin;
-        Vector2 rayDirection = new Vector2 (direction,0);
+        Vector2 rayDirection = new Vector2(direction, 0);
 
+        //Determine if moving left or right and decide where to start ray iteration
         if (direction > 0)
         {
             rayOrigin = raycastOrigins.bottomRight;
@@ -72,39 +73,80 @@ public class MovementCollisionHandler : MonoBehaviour
             rayOrigin = raycastOrigins.bottomLeft;
         }
 
-
-
+        //For loop runs however many times we have set rays to be cast in horizontal direction
         for (int i = 0; i < xRayCount; i++)
         {
-            RaycastHit2D collision = Physics2D.Raycast(rayOrigin + Vector2.up * xRaySpacing * i, rayDirection, distanceToCast, LayerMask.GetMask("Solid"));
-
-            if (collision.collider)
+            RaycastHit2D collision = Physics2D.Raycast(rayOrigin + i * xRaySpacing * Vector2.up, rayDirection, distanceToCast, LayerMask.GetMask("Solid"));
+            //If there is a collision we will update velocity.x accordingly and decrease distance to cast as well so we dont cast subsequent rays too far and move into a block
+            if (collision.collider & collision.collider)
             {
-                Debug.Log("Hitting " + collision.collider.name + " at a distance of " + collision.distance);
-                velocity.x = collision.distance * direction;
+                
+                velocity.x = (collision.distance- 0.01f) * direction;
                 distanceToCast = collision.distance;
             }
         }
-
-
     }
+
+    void CollisionsYAxis(ref Vector3 velocity) {
+        float yVelocity = velocity.y;
+        if (yVelocity == 0) return;
+        float direction = Mathf.Sign(yVelocity);
+        float distanceToCast = Mathf.Abs(yVelocity);
+
+        Vector2 rayOrigin;
+        Vector2 rayDirection = new Vector2(0, direction);
+
+        //Determine if moving up or down and decide where to start ray iteration
+        if (direction > 0)
+        {
+            rayOrigin = raycastOrigins.topLeft;
+        }
+        else
+        {
+            rayOrigin = raycastOrigins.bottomLeft;
+        }
+
+        //For loop runs however many times we have set rays to be cast in vertical direction
+        for (int i = 0; i < yRayCount; i++)
+        {
+            RaycastHit2D collision = Physics2D.Raycast(rayOrigin + i * yRaySpacing * Vector2.right, rayDirection, distanceToCast, LayerMask.GetMask("Solid"));
+            
+            //If there is a collision we will update velocity.y accordingly and decrease distance to cast as well so we dont cast subsequent rays too far and move into a block
+            if (collision.collider)
+            {
+                
+                velocity.y = (collision.distance- 0.01f) * direction;
+                distanceToCast = collision.distance;
+            }
+        }
+    }
+
+
+    //Calculating Spacing and Locations for Rays to Be Cast
+    //====================================================================================================
 
     private void updateRaycastOrigins()
     {
-        raycastOrigins.topRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
-        raycastOrigins.topLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
-        raycastOrigins.bottomRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
-        raycastOrigins.bottomLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+        Bounds bounds = boxCollider.bounds;
+        bounds.Expand (0.01f * -2);
+
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
     }
 
     private void updateRaySpacing()
     {
+
+        Bounds bounds = boxCollider.bounds;
+        bounds.Expand (0.01f * -2);
         //We need at least 2 rays per side
         xRayCount = Mathf.Clamp(xRayCount, 2, int.MaxValue);
         yRayCount = Mathf.Clamp(yRayCount, 2, int.MaxValue);
 
-        xRaySpacing = boxCollider.bounds.size.x / (xRayCount - 1);
-        yRaySpacing = boxCollider.bounds.size.y / (yRayCount - 1);
+        xRaySpacing = bounds.size.x / (xRayCount - 1);
+        yRaySpacing = bounds.size.y / (yRayCount - 1);
     }
 
     struct RaycastOrigins
