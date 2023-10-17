d9cc909f765daeed99c87d5d51d1cd23e0d4c436
Will <williambolls98@gmail.com>
2023-10-16 17:19:13 -0700

    refactored a bit so that collision and movement handler extends a new raycast class

--- Assets/Scripts/MovementCollisionHandler.cs
+++ Assets/Scripts/MovementCollisionHandler.cs
@@ -2,30 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class MovementCollisionHandler : MonoBehaviour
+
+public class MovementCollisionHandler : RaycastController
 {
     //Define
     //====================================================================================================================
-    private BoxCollider2D boxCollider;
 
-    // [SerializeField]
-    // private Vector3 velocity;
-    [Header("Raycast Settings")]
-    [SerializeField]
-    private int xRayCount = 4;
 
-    [SerializeField]
-    private int yRayCount = 4;
-
-    [SerializeField]
-    private float skinWidth = 0.001f;
-
-    private float verticalRaySpacing;
-
-    private float horizontalRaySpacing;
-
-    private RaycastOrigins raycastOrigins;
+    
 
     public CollisionInfo collisionInfo;
 
@@ -35,11 +19,7 @@ public class MovementCollisionHandler : MonoBehaviour
 
     //====================================================================================================================
     // Start is called before the first frame update
-    void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-        updateRaySpacing();
-    }
+
 
     public void Move(Vector3 velocity)
     {
@@ -164,50 +144,12 @@ public class MovementCollisionHandler : MonoBehaviour
     }
 
 
-    //Calculating Spacing and Locations for Rays to Be Cast
-    //====================================================================================================
-    private void updateRaycastOrigins()
-    {
-        Bounds bounds = boxCollider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-    }
-
-    private void addToRaycastOriginsX(float ammountToAdd) {
-        Bounds bounds = boxCollider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        raycastOrigins.topRight = new Vector2(bounds.max.x + ammountToAdd, bounds.max.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x+ ammountToAdd, bounds.max.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x+ ammountToAdd, bounds.min.y);
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x+ ammountToAdd, bounds.min.y);
-    }
-
-    private void updateRaySpacing()
-    {
-        Bounds bounds = boxCollider.bounds;
-        bounds.Expand(skinWidth * -2);
-        //We need at least 2 rays per side
-        xRayCount = Mathf.Clamp(xRayCount, 2, int.MaxValue);
-        yRayCount = Mathf.Clamp(yRayCount, 2, int.MaxValue);
-
-        verticalRaySpacing = bounds.size.x / (xRayCount - 1);
-        horizontalRaySpacing = bounds.size.y / (yRayCount - 1);
-    }
-
+   
 //Structs
 //==============================================================================
 
 
-    struct RaycastOrigins
-    {
-        public Vector2 topLeft, topRight;
-        public Vector2 bottomLeft, bottomRight;
-    }
+    
 
     public struct CollisionInfo
     {
