24447d3c0d647d0de33d659c5e8a4b57f8f2f4ea
Will <williambolls98@gmail.com>
2023-12-02 21:24:37 -0800

    camera can be bound within a tilemap

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraControls : MonoBehaviour
 {
@@ -8,33 +9,59 @@ public class CameraControls : MonoBehaviour
     [SerializeField]
     private Transform target;
 
+    
+
     [SerializeField]
     private Vector3 offset;
 
+    [Header ("Easing")]
     [SerializeField]
     private float easeTimeX = 0.2f;
     [SerializeField]
     private float easeTimeY = 0.1f;
 
+    [Header ("Binding")]
+    [SerializeField]
+    private Tilemap tilemap;
+    [SerializeField]
+    private float boarderBuffer = 0.5f;
+
     private Vector3 velocity = Vector3.zero;
+
+    void Start() {
+        transform.position = target.position + offset;
+    }
 
     void FixedUpdate()
     {
-        // transform.position = new Vector3(target.position.x, target.position.y, transform.position.z) + offset;
+        EaseToTarget(target);
+    }
+
+    private void EaseToTarget(Transform target)
+    {
         Vector3 targetPosition = target.position + offset;
 
-        Vector3 xVector = new(transform.position.x,0,0);
-        Vector3 targetXVector = new(targetPosition.x,0,0);
+        Vector3 xVector = new(transform.position.x, 0, 0);
+        Vector3 targetXVector = new(targetPosition.x, 0, 0);
 
-        Vector3 yVector = new(0,transform.position.y,0);
-        Vector3 targetYVector = new(0,targetPosition.y,0);
+        Vector3 yVector = new(0, transform.position.y, 0);
+        Vector3 targetYVector = new(0, targetPosition.y, 0);
+        
+        float xTarget = Vector3.SmoothDamp(xVector, targetXVector, ref velocity, easeTimeX).x;
+        float yTarget = Vector3.SmoothDamp(yVector, targetYVector, ref velocity, easeTimeY).y;
 
-        yVector = Vector3.SmoothDamp(yVector, targetYVector, ref velocity, easeTimeY);
-        xVector = Vector3.SmoothDamp(xVector, targetXVector, ref velocity, easeTimeX);
+        if (tilemap) {
+            Camera camera = Camera.main;
+            float verticalExtent = camera.orthographicSize+boarderBuffer;
+            float horizontalExtent = (verticalExtent * camera.aspect)+boarderBuffer;
 
-        transform.position = new(xVector.x,yVector.y,transform.position.z);
-        // transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, easeTime);
+            Vector3 minPoint = tilemap.localBounds.min + new Vector3(horizontalExtent,verticalExtent,0);
+            Vector3 maxPoint = tilemap.localBounds.max - new Vector3(horizontalExtent,verticalExtent,0);
 
+            xTarget = Mathf.Clamp(xTarget,minPoint.x,maxPoint.x);
+            yTarget = Mathf.Clamp(yTarget,minPoint.y,maxPoint.y); 
+        }
 
+        transform.position = new(xTarget, yTarget, transform.position.z);
     }
 }
