2d3c529ad835e122f7fbbe1932aeb3934fe28e88
Will <williambolls98@gmail.com>
2023-12-02 20:57:40 -0800

    seperated out the x and y easing for the camera

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -12,21 +12,29 @@ public class CameraControls : MonoBehaviour
     private Vector3 offset;
 
     [SerializeField]
-    private float easeTime;
+    private float easeTimeX = 0.2f;
+    [SerializeField]
+    private float easeTimeY = 0.1f;
 
     private Vector3 velocity = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         // transform.position = new Vector3(target.position.x, target.position.y, transform.position.z) + offset;
         Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, easeTime);
+
+        Vector3 xVector = new(transform.position.x,0,0);
+        Vector3 targetXVector = new(targetPosition.x,0,0);
+
+        Vector3 yVector = new(0,transform.position.y,0);
+        Vector3 targetYVector = new(0,targetPosition.y,0);
+
+        yVector = Vector3.SmoothDamp(yVector, targetYVector, ref velocity, easeTimeY);
+        xVector = Vector3.SmoothDamp(xVector, targetXVector, ref velocity, easeTimeX);
+
+        transform.position = new(xVector.x,yVector.y,transform.position.z);
+        // transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, easeTime);
+
+
     }
 }
