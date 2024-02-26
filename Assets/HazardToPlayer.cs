ada077a533f5a5b4155745bbf159afc63b43cf12
Will <williambolls98@gmail.com>
2024-02-25 22:32:26 -0800

    added spikes

---
+++ Assets/HazardToPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardToPlayer : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Player hitPlayer = player.GetComponent<Player>();
+
+            if (!hitPlayer.IsInvincible())
+            {
+                int directionToShove = (player.transform.position.x > transform.position.x) ? 1 : -1;
+                GameController.Instance.ChangeHealth(-1);
+                hitPlayer.Shove(directionToShove);
+            }
+        }
+    }
+}
