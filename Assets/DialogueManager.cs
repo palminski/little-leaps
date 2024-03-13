e1b45405da970ce423c5dc00eda3a50ae5a9e53c
Will <williambolls98@gmail.com>
2024-03-12 21:11:40 -0700

    dialogue options can now be selected by the player and can launch new dialogues when selected

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField]
     public TMP_Text textElement;
+    [SerializeField]
+    public TMP_Text optionsElement;
 
     [SerializeField]
     public float timeBetweenCharacters = 0.05f;
@@ -14,27 +17,108 @@ public class DialogueManager : MonoBehaviour
 
     private int currentIndex;
 
+    private int dialogueOptionIndex;
+
+    private PlayerInput playerInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        textElement = GetComponentInChildren<TMP_Text>();
+
         currentIndex = 0;
         StartCoroutine(TypeSentence(currentDialogue.dialogueSentences[currentIndex].text));
+        playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        playerInput.enabled = false;
+
+    }
+
+    void OnDestroy()
+    {
+        if (playerInput) playerInput.enabled = true;
     }
 
     void OnSelect()
     {
-        if (IsTyping()) {
+        if (IsTyping())
+        {
             CompleteSentence();
             return;
+        }
+        if (AreDialogueOptionsVisable()) 
+        {
+            DialogueOption dialogueOption = currentDialogue.dialogueSentences[currentIndex].dialogueOptions[dialogueOptionIndex];
+
+            if(dialogueOption.nextDialogue) 
+            {
+                currentDialogue = dialogueOption.nextDialogue;
+                currentIndex = 0;
+                StartCoroutine(TypeSentence(currentDialogue.dialogueSentences[currentIndex].text));
+                return;
+            }
         }
         DisplayNextSentence();
     }
 
+    void OnNavigateLeft() {
+        if (AreDialogueOptionsVisable()) UpdateOptionIndex(-1);
+    }
 
-    private void DisplayNextSentence() {
+    void OnNavigateRight() {
+        if (AreDialogueOptionsVisable()) UpdateOptionIndex(1);
+    }
+
+    private void UpdateOptionIndex(int ammount) {
+        print("navigate");
+        int maxIndex = currentDialogue.dialogueSentences[currentIndex].dialogueOptions.Count - 1;
+        optionsElement.text = "";
+        if (dialogueOptionIndex + ammount > maxIndex)
+        {
+            dialogueOptionIndex = 0;
+        }
+        else if (dialogueOptionIndex + ammount < 0)
+        {
+            dialogueOptionIndex = maxIndex;
+        }
+        else
+        {
+            dialogueOptionIndex += ammount;
+        }
+        UpdateDialogueOptions();
+    }
+
+    private void DisplayDialogueOptions()
+    {
+        List<DialogueOption> dialogueOptions = currentDialogue.dialogueSentences[currentIndex].dialogueOptions;
+
+        dialogueOptionIndex = 0;
+
+        
+        optionsElement.text = "";
+
+        for (int i = 0; i < dialogueOptions.Count; i++)
+        {
+            optionsElement.text += i == dialogueOptionIndex ? $"[{dialogueOptions[i].option}]" : dialogueOptions[i].option;
+            if (i != dialogueOptions.Count - 1) optionsElement.text += "     ";
+        }
+    }
+
+    private void UpdateDialogueOptions()
+    {
+        List<DialogueOption> dialogueOptions = currentDialogue.dialogueSentences[currentIndex].dialogueOptions;
+        for (int i = 0; i < dialogueOptions.Count; i++)
+        {
+            optionsElement.text += i == dialogueOptionIndex ? $"[{dialogueOptions[i].option}]" : dialogueOptions[i].option;
+            if (i != dialogueOptions.Count - 1) optionsElement.text += "     ";
+        }
+    }
+
+
+
+    private void DisplayNextSentence()
+    {
         int lastIndex = currentDialogue.dialogueSentences.Count - 1;
-        if (currentIndex == lastIndex) {
+        if (currentIndex == lastIndex)
+        {
             Destroy(gameObject);
             return;
         }
@@ -42,20 +126,32 @@ public class DialogueManager : MonoBehaviour
         StartCoroutine(TypeSentence(currentDialogue.dialogueSentences[currentIndex].text));
     }
 
-    IEnumerator TypeSentence(string sentence) {
+    IEnumerator TypeSentence(string sentence)
+    {
         textElement.text = "";
-        foreach(char character in sentence.ToCharArray())
+        optionsElement.text = "";
+        foreach (char character in sentence.ToCharArray())
         {
             textElement.text += character;
             yield return new WaitForSeconds(timeBetweenCharacters);
         }
+        if (currentDialogue.dialogueSentences[currentIndex].dialogueOptions.Count > 0) DisplayDialogueOptions();
     }
-    private void CompleteSentence() {
+    private void CompleteSentence()
+    {
         StopAllCoroutines();
-        textElement.text = currentDialogue.dialogueSentences[currentIndex].text; 
+        textElement.text = currentDialogue.dialogueSentences[currentIndex].text;
+        if (currentDialogue.dialogueSentences[currentIndex].dialogueOptions.Count > 0) DisplayDialogueOptions();
     }
 
-    private bool IsTyping() {
+    private bool IsTyping()
+    {
         return textElement.text != currentDialogue.dialogueSentences[currentIndex].text;
+    }
+
+    private bool AreDialogueOptionsVisable()
+    {
+        if (!IsTyping() && currentDialogue.dialogueSentences[currentIndex].dialogueOptions.Count > 0) return true;
+        return false;
     }
 }
