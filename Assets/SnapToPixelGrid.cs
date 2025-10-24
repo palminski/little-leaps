39098188b9ced6a84b680fc6bf8415d9ab114caf
Will <williambolls98@gmail.com>
2025-10-24 11:23:10 -0700

    removed a package

--- Assets/SnapToPixelGrid.cs
+++ Assets/SnapToPixelGrid.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class SnapToPixelGrid : MonoBehaviour
 {
-    private PixelPerfectCamera ppc;
+    private UnityEngine.Rendering.Universal.PixelPerfectCamera ppc;
     // Start is called before the first frame update
     void Awake()
     {
-        ppc = GetComponent<PixelPerfectCamera>();
+        ppc = GetComponent<UnityEngine.Rendering.Universal.PixelPerfectCamera>();
     }
 
     // Update is called once per frame
